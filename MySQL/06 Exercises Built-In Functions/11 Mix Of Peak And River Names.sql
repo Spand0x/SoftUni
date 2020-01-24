8300b09bbb1a3466d92b2667be53bf3f17bd07a4
Spand0x <gexna97@gmail.com>
2020-01-24 16:12:54 +0200

    Updated Exercises From Built-In Functions

--- MySQL/06 Exercises Built-In Functions/11 Mix Of Peak And River Names.sql
+++ MySQL/06 Exercises Built-In Functions/11 Mix Of Peak And River Names.sql
@@ -3,4 +3,5 @@ SELECT peak_name,
        river_name,
        LOWER(CONCAT(peak_name,SUBSTRING(river_name,2))) AS MIX
        FROM peaks,rivers
-    WHERE RIGHT(peak_name,1) = LEFT(river_name,1);
+    WHERE LOWER(RIGHT(peak_name,1)) = LOWER(LEFT(river_name,1))
+    ORDER BY MIX;
